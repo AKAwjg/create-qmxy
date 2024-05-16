c108ca166fd460940187b924f5e19bc84ea94542
奇妙小羊 <14409538+wonderful-little-lamb@user.noreply.gitee.com>
2024-05-16 18:24:00 +0800

    修改了一些细节

--- template/src/router/index.ts
+++ template/src/router/index.ts
@@ -1,11 +1,11 @@
 //通过vue-router插件实现模板路由配置
 import { createRouter, createWebHistory } from 'vue-router';
-import { constantRoute } from './routes'
+import { constantRoutes } from './routes'
 
 //创建路由器
 let router = createRouter({
     history: createWebHistory(),
-    routes: constantRoute,
+    routes: constantRoutes,
     //滚动行为
     scrollBehavior() {
         return {
